6ac8c32b988e27c0b66743f34e963ad2e8261115
azez0 <nanquery@gmail.com>
2023-03-03 10:43:28 +0100

    refactor code

--- trip_management/admin.py
+++ trip_management/admin.py
@@ -1,6 +1,10 @@
+from .models.user import User
 from django.contrib import admin
-from .models import *
-# Register your models here.
+from .models.review import Review
+from .models.address import Address
+from .models.location import Location
+from .models.response import Response
+from .models.favorite import Favorite
 
 admin.site.register(User)
 admin.site.register(Address)
@@ -8,5 +12,3 @@ admin.site.register(Location)
 admin.site.register(Review)
 admin.site.register(Response)
 admin.site.register(Favorite)
-
-
